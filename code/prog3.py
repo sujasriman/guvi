6cb9a804025fe48032a2cc3a047de5e4e54b1ba4
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-04 15:39:24 +0530

    Update prog3.py

--- code/prog3.py
+++ code/prog3.py
@@ -4,6 +4,6 @@ if((b>=97 and b<=122) or (b>=65 and b<=90)):
     if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u' or a=='A' or a=='E' or a=='I' or a=='O' or a=='U'):
         print("Vowel")
     else:
-        print("Consonent")
+        print("Consonant")
 else:
     print("Invalid")
