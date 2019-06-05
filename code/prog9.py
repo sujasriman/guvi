4bb236a84ae27dbce4318c83d9b8331d0cad3a97
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-05 13:40:11 +0530

    Update prog9.py

--- code/prog9.py
+++ code/prog9.py
@@ -5,8 +5,11 @@ s=0
 if(N>0):
     for i in range(1,N+1):
         print(i)
-    for i in range(1,K+1):
-        s=s+i
-    print(s)
+    if(K>0 and K<=N):
+        for i in range(1,K+1):
+            s=s+i
+        print(s)
+    else:
+        print("invalid")
 else:
     print("invalid")
