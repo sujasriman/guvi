8db1f4846481e4bac7b043c84be30fafa694f44d
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-05 14:34:17 +0530

    Update prog13.py

--- code/prog13.py
+++ code/prog13.py
@@ -2,11 +2,11 @@ a=int(input())
 count=0
 if(a>0):
    for i in range(1,a+1):
-    if(a%i==0):
-        count=count+1
-    if(count==2):
-        print("yes")
-    else:
-        print("no") 
+       if(a%i==0):
+           count=count+1
 else:
-    print("invalid")
+    print("invalid")    
+if(count==2):
+    print("yes")
+else:
+    print("no") 
