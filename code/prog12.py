a8cac198bf8bbbf4e21422e052ef081ff5f7c51e
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-05 14:22:52 +0530

    Update prog12.py

--- code/prog12.py
+++ code/prog12.py
@@ -1,15 +1,12 @@
 a=int(input())
 temp=a
 b=0
-if(a<=1000):
-    while(a>0):
-        rem=a%10
-        b=(b*10)+rem
-        a=a/10
-        a=int(a)
-    if(b==temp):
-        print("yes")
-    else:
-        print("no")
+while(a>0):
+    rem=a%10
+    b=(b*10)+rem
+    a=a/10
+    a=int(a)
+if(b==temp):
+    print("yes")
 else:
-    print("invalid")
+    print("no")
