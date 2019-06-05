284a13b9b3487035da813b101689c958b62f4649
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-05 15:44:55 +0530

    Update prog19.py

--- code/prog19.py
+++ code/prog19.py
@@ -1,5 +1,11 @@
 a=int(input())
 fact=1
-for i in range(1,a+1):
-    fact=fact*i
-print(fact)
+if(a>0):
+    for i in range(1,a+1):
+        fact=fact*i
+    print(fact)
+elif(a==0):
+    fact=1
+    print(fact)
+else:
+    print("invalid")
