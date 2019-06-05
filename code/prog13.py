476d4818a8ea082ed1f831a8a9eba8e58221e613
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-05 14:30:30 +0530

    Create prog13.py

---
+++ code/prog13.py
@@ -0,0 +1,12 @@
+a=int(input())
+count=0
+if(a>0):
+   for i in range(1,a+1):
+    if(a%i==0):
+        count=count+1
+    if(count==2):
+        print("yes")
+    else:
+        print("no") 
+else:
+    print("invalid")
