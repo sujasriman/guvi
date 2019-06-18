9170fd6fc70d18cc8d019dff9e865c3f635a131d
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-18 21:56:54 +0530

    Create pprog194.py

---
+++ code/pprog194.py
@@ -0,0 +1,10 @@
+def r_p_s(a,b):
+    if((a=='R' and b=='P')or(a=='P' and b=='R')):
+        return 'P'
+    elif((a=='R' and b=='S')or(a=='S' and b=='R')):
+        return 'R'
+    elif((a=='S' and b=='P')or(a=='P' and b=='S')):
+        return 'S'
+c,d=input().split()
+ans=r_p_s(c,d)
+print(ans)
