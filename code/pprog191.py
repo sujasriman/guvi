a63f845c618565bec6cca5694a22b6356e4e5f41
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-18 21:27:27 +0530

    Update pprog191.py

--- code/pprog191.py
+++ code/pprog191.py
@@ -1,13 +1,11 @@
 n=int(input())
-l=[]
 l1=[]
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
-l=b
 for i in range(n-1,-1,-1):
     c=a[i]
     l1.append(c)
-if(l==l1):
+if(b==l1):
     print("yes")
 else:
     print("no")
