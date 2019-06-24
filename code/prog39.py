bf5408a2dd11a9695cf0ca77ec22cd5d2ae47fcc
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-24 20:36:08 +0530

    Update prog39.py

--- code/prog39.py
+++ code/prog39.py
@@ -1,2 +1,7 @@
-b=list(map(int,input().split()))
-print(max(b))
+l=list(map(int,input().split()))
+b=l[0]
+for i in range(10):
+    for j in range(i+1,10):
+        if(b<l[j]):
+            b=l[j]
+print(b)
