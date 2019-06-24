9d0035894b87fc8843f45bcd13c85b297057eef8
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-24 18:28:10 +0530

    Update prog23.py

--- code/prog23.py
+++ code/prog23.py
@@ -1,6 +1,8 @@
-n=input()
-n=int(n)
+n=int(input())
 l=list(map(int,input().split()))
-l.sort()
-a=l[0]
-print(a)
+b=l[0]
+for i in range(n):
+    for j in range(i+1,n):
+        if(b>l[j]):
+            b=l[j]
+print(b)
