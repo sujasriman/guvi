056c1bd952ecc4e34a9e48e3abff0007faae547d
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-24 21:37:28 +0530

    Update prog50.py

--- code/prog50.py
+++ code/prog50.py
@@ -1,6 +1,12 @@
-a=int(input())
-if(a%2==0):
+n=int(input())
+n=n//2
+c=0
+for i in range(n):
+    if(n==2**i):
+        c+=1
+        break
+if(c==1):
     print("yes")
 else:
     print("no")
-    
+        
