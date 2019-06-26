cd315166ed7774ee22adebaf3041d58e958cd329
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-26 21:45:01 +0530

    Update pprog172.py

--- code/pprog172.py
+++ code/pprog172.py
@@ -1,10 +1,13 @@
 n=int(input())
 l=[]
+l1=[]
 while(n>0):
     rem=n%10
     l.append(rem)
     n=n//10
 l.sort(reverse=True)
-for i in range(len(l)-1):
-    print(l[i],end='')
-print(l[len(l)-1])
+for i in range(len(l)):
+    l1.append(l[i])
+for i in range(len(l1)-1):
+    print(l1[i],end='')
+print(l1[len(l)-1])
