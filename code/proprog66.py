7b2fd55c2e910bbca57ce4a03db7daf6e182a858
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-27 20:02:41 +0530

    Create proprog66.py

---
+++ code/proprog66.py
@@ -0,0 +1,18 @@
+a,b,f,k=input().split()
+a=int(a)
+b=int(b)
+f=int(f)
+k=int(k)
+distance=a*k
+c=b
+count=0
+if(a<b and a>f):
+    for i in range(0,(2*k)):
+        c=c-a
+        if(c<0):
+            c=b
+            count+=1
+    print(count)
+else:
+    count=-1
+    print(count)
