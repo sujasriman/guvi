2b5157e2248addba68baa547ef807bae9c90a1c2
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-29 08:53:23 +0530

    Create pprog130.py

---
+++ code/pprog130.py
@@ -0,0 +1,14 @@
+n=int(input())
+l1=[]
+l=list(map(int,input().split()))
+for i in range(n):
+    s=0
+    if(l[i]%2==0):
+        for j in range(i+1):
+            s=s+l[j]
+        l1.append(s)
+    else:
+        l1.append(l[i])
+for i in range(len(l1)-1):
+    print(l1[i],end=' ')
+print(l1[len(l)-1])
