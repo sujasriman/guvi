b0cfbf013dacf52b0dd5e49478f2153b90f71cd8
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-29 23:11:45 +0530

    Create pprog23.py

---
+++ code/pprog23.py
@@ -0,0 +1,15 @@
+n,k=input().split()
+n=int(n)
+k=int(k)
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l3=[]
+for i in range(k):
+    l1.append(l2[i])
+    l3.append(max(l1))
+if(len(l3)==1):
+    print(l3[0])
+else:
+    for i in range(k-1):
+        print(l3[i],end=' ')
+    print(l3[k-1])
