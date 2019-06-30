78b10e0c4e4df0517c182e6025a7da0f3dce73d8
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-30 11:06:24 +0530

    Update pprog55.py

--- code/pprog55.py
+++ code/pprog55.py
@@ -2,7 +2,7 @@ s1,s2=input().split()
 count=0
 if(len(s1)==len(s2)):
     for i in range(len(s1)):
-        if(s[i].isupper()):
+        if(s1[i].isupper()):
             if(ord(s1[i])==ord(s2[i]) or ord(s1[i])==ord(s2[i])-32):
                count+=1
         else:
