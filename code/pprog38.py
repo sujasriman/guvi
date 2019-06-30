49ee540eca988a13abb8163f6bb4539a44aad9ce
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-06-30 08:58:29 +0530

    Create pprog38.py

---
+++ code/pprog38.py
@@ -0,0 +1,15 @@
+n=int(input())
+l=[]
+l1=[]
+for i in range(1,n+1):
+    if(n%i==0):
+        l.append(i)
+for i in range(len(l)):
+    if(l[i]%2==0):
+        l1.append(l[i])
+if(len(l1)==1):
+    print(l1[0])
+else:
+    for i in range(len(l1)-1):
+        print(l1[i],end=' ')
+    print(l1[len(l1)-1])
