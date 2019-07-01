82277465d4c9e4c59aa560deff78b633e5c75daa
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-07-01 19:41:54 +0530

    Update pprog109.py

--- code/pprog109.py
+++ code/pprog109.py
@@ -2,15 +2,10 @@ n=int(input())
 l=list(map(int,input().split()))
 s=0
 l1=[]
-l2=[]
 for i in range(n):
     s=s+l[i]
     l1.append(s)
-for i in range(n-1,-1,-1):
-    l2.append(l1[i])
-if(n>1):
-    for i in range(len(l1)):
-        l1[i]=l1[i]+l2[i]
+l1.reverse()
 if(len(l1)==1):
     print(l1[0])
 else:
