6b51b70be1fc1c26e9d99ee24ec038b483417bcc
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-07-28 22:18:23 +0530

    Update hprog57.py

--- code/hprog57.py
+++ code/hprog57.py
@@ -1,6 +1,11 @@
 n=int(input())
+count=0
+b=0
 l=list(map(int,input().split()))
 for i in range(n):
     if(l.count(l[i])==1):
-        print(l[i])
+        count+=1
+        b=l[i]
         break
+if(count>0):
+    print(b)
