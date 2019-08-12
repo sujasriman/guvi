2c377be073827f9d0bbacf0e76379b01d3676dc7
sujasriman <51361224+sujasriman@users.noreply.github.com>
2019-08-12 20:14:03 +0530

    Update prog63.py

--- code/prog63.py
+++ code/prog63.py
@@ -1,8 +1,6 @@
 l=list(map(int,input().split()))
-a=0
-for i in range(len(l)-1):
-    if(l[i]<=l[i+1]):
+a=l[0]
+for i in range(1,len(l)):
+    if(a>l[i]):
         a=l[i]
-    else:
-        a=l[i+1]
 print(a)
